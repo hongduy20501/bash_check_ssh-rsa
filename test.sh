a340d05d8c2f8829e280a2935bf941d71abf9b4c
Lê Hồng Duy <72614920+hongduy20501@users.noreply.github.com>
2023-07-04 11:55:35 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,35 +1,33 @@
 #!/usr/bin/env bash
-set -e
-
+set -ex
 for item in $(sudo cat /etc/passwd | grep -v "nologin" | grep -v "false" | cut -d: -f1)
 do
- if [ -f $(eval echo ~$item)/.ssh/authorized_keys ]; then
-    echo "${item}"
- 	sudo cat $(eval echo ~$item)/.ssh/authorized_keys | grep -oE "ssh-rsa [A-Za-z0-9/+=]+" | while read line; do
- 	  echo "${line}" | md5sum | grep -oE "[A-Fa-f0-9]+" >> keys.list 
- 	done
- fi
-done
-
+	if [ -f $(eval echo ~$item)/.ssh/authorized_keys ]; then
+                echo "${item}"
+                sudo cat $(eval echo ~$item)/.ssh/authorized_keys | grep -oE "ssh-rsa [A-Za-z0-9/+=]+" >> keys.list
+        fi
+        done
+while read -r linekey; do
+        a=$(echo "${linekey}" | md5sum | grep -oE "[A-Fa-f0-9]+")
+        flag=0
 while read -r line; do
-	a=$(echo "${line}" | cut -d' ' -f1)
-	b=$(echo -n "${line}" | grep -oE "ssh-rsa [A-Za-z0-9/+=]+")
-        c=$(echo "${b}" | md5sum | grep -oE "[A-Fa-f0-9]+")
-        while read -r linekey; do
-                if [[  ${c} == ${linekey} ]]; then
-                        echo " key: ${b} thuoc user: ${a}"
-		fi
-	done < keys.list
-done < keys
+        b=$(echo "${line}" | cut -d' ' -f1)
+        c=$(echo -n "${line}" | grep -oE "ssh-rsa [A-Za-z0-9/+=]+")
+        d=$(echo "${c}" | md5sum | grep -oE "[A-Fa-f0-9]+")
 
+        if [[ ${a} == ${d} ]]; then
+                flag=1
+                echo "user: ${b}" >> login.list
+                break
+        fi
+        done < keys
+        if [ ${flag} == 0 ]; then
+                echo "key: ${linekey}" >> nologin.list
+        fi
+done < keys.list
 
-#while read -r linekey; do
-#	while read -r lineuser; do
-#	a=$(echo "${lineuser}"| cut -d' ' -f2)
-#	if [[ ${linekey} == ${a} ]]; then
-#		b=$(echo "${lineuser}" | cut -d' ' -f1)
-#		echo " key : ${linekey} thuoc user: ${b}"
-#	fi
-#	done < user.list
-#done < keys.list
-		
+echo "user co the login :"
+cat -n login.list
+echo "-------------------------------"
+echo "key khong thuoc user nao :"
+cat -n nologin.list
